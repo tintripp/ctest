79683ea460bf5b8600a071f343d6512ea362e6df
Timothy Tripp <ti.n.tripp@baypath.net>
2025-09-23 17:56:26 -0400

    Collision, frame independence, john

--- include/constants.h
+++ include/constants.h
@@ -4,7 +4,7 @@ const int SCREEN_WIDTH = 320;
 const int SCREEN_HEIGHT = 240;
 const int TILE_SIZE = 16;
 
-const int WINDOW_SCALE = 2;
+const int WINDOW_SCALE = 3;
 const char* WINDOW_TITLE = "Ctest";
 
 const int FPS = 30;
--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <stdbool.h>
 #include <SDL.h>
 #include <SDL_main.h>
+#include <SDL_image.h>
 #include "constants.h"
 
 #define LEVEL_WIDTH 20
@@ -21,49 +22,110 @@ typedef struct {
 } Player;
 
 //global variables
-SDL_Window* window;
-SDL_Renderer* renderer;
-SDL_Texture* screen;
-SDL_Event e;
-bool done = false;
+typedef struct {
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    SDL_Texture* screen;
+    bool done;
 
-//beta
+    Tile levelTiles[LEVEL_WIDTH][LEVEL_HEIGHT];
+    Player player;
 
-//FIND A BETTER PLACE FOR THIS
-Tile levelTiles[LEVEL_WIDTH][LEVEL_HEIGHT];
-
-SDL_FRect player = {0, 0, 16, 16};
+} GameContext;
 
 //functions
-void handleEvents() {
-    while (SDL_PollEvent(&e)){
-        if (e.type == SDL_QUIT) {
-            done = true;
+void init(GameContext* ctx) {
+    //init
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    ctx->done = false;
+
+    ctx->window = SDL_CreateWindow(
+        WINDOW_TITLE, 
+        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+        SCREEN_WIDTH * WINDOW_SCALE, 
+        SCREEN_HEIGHT * WINDOW_SCALE, 
+        0
+    );
+    ctx->renderer = SDL_CreateRenderer(ctx->window, 0, 0);
+    ctx->screen = SDL_CreateTexture(
+        ctx->renderer, 
+        SDL_PIXELFORMAT_RGBA8888, 
+        SDL_TEXTUREACCESS_TARGET, 
+        SCREEN_WIDTH, SCREEN_HEIGHT
+    );
+}
+void handleEvents(GameContext* ctx) {
+    SDL_Event event;
+    while (SDL_PollEvent( &event )){
+        if (event.type == SDL_QUIT) {
+            ctx->done = true;
+        }
+        if (event.type == SDL_KEYDOWN) {
+            if(event.key.keysym.sym == SDLK_SPACE)
+                ctx->player.yVelocity = -200;
         }
     }
 }
-void update(float deltaTime) {
+void update(GameContext* ctx, float deltaTime) {
     const Uint8* state = SDL_GetKeyboardState(NULL);
 
-    if (state[SDL_SCANCODE_W]) {
-        player.x += 100 * deltaTime;
+        printf("%f\n",ctx->player.yVelocity);
+
+    ctx->player.yVelocity += 300 * deltaTime;
+
+
+    if (state[SDL_SCANCODE_D]) {
+        ctx->player.hitbox.x += 100 * deltaTime;
     }
-    if (state[SDL_SCANCODE_SPACE]) {
-        player.x -= 100 * deltaTime;
+    if (state[SDL_SCANCODE_A]) {
+        ctx->player.hitbox.x -= 100 * deltaTime;
+    }
+
+    ctx->player.hitbox.y += ctx->player.yVelocity * deltaTime;
+    //now check for collisions between plr and tiles.
+    for (int tRow = 0; tRow < LEVEL_HEIGHT; tRow ++) {
+
+        for (int tCol = 0; tCol < LEVEL_WIDTH; tCol ++) {
+
+            if (ctx->levelTiles[tCol][tRow].id == 1) { // 1=solid/existing for now
+                SDL_FRect tileRect = {
+                    tCol*TILE_SIZE,
+                    tRow*TILE_SIZE,
+                    TILE_SIZE,TILE_SIZE
+                };
+
+                if (SDL_HasIntersectionF(
+                    &ctx->player.hitbox, 
+                    &tileRect
+                )){
+                    if ((tileRect.y-ctx->player.hitbox.y) > 0) {
+                        ctx->player.hitbox.y = tileRect.y - ctx->player.hitbox.h;
+                    } else{
+                        ctx->player.hitbox.y = tileRect.y + tileRect.h;
+                    }
+                    ctx->player.yVelocity = 0;
+                }
+            }
+            
+        }
+
     }
 }
-void draw() {
-    SDL_SetRenderTarget(renderer, screen);
+void draw(GameContext* ctx) {
+    SDL_SetRenderTarget(ctx->renderer, ctx->screen);
 
-    SDL_SetRenderDrawColor(renderer, 127, 127, 127, 255);
-    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(ctx->renderer, 127, 127, 127, 255);
+    SDL_RenderClear(ctx->renderer);
 
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_SetRenderDrawColor(ctx->renderer, 0, 0, 0, 255);
     for (int tRow = 0; tRow < LEVEL_HEIGHT; tRow ++) {
+
         for (int tCol = 0; tCol < LEVEL_WIDTH; tCol ++) {
-            if (levelTiles[tCol][tRow].id == 1) { // solid/existing for now
+
+            if (ctx->levelTiles[tCol][tRow].id == 1) { // 1=solid/existing for now
                 SDL_RenderFillRect(
-                    renderer, 
+                    ctx->renderer, 
                     &(SDL_Rect){
                         tCol*TILE_SIZE,
                         tRow*TILE_SIZE,
@@ -71,59 +133,71 @@ void draw() {
                     }
                 );
             }
+            
         }
+
     }
 
-    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
-    SDL_RenderFillRectF(renderer, &player);
+    SDL_SetRenderDrawColor(ctx->renderer, 0, 0, 255, 255);
+    SDL_RenderFillRectF(ctx->renderer, &ctx->player.hitbox);
 
-    SDL_SetRenderTarget(renderer, NULL);
-    SDL_RenderCopy(renderer, screen, NULL, NULL);
+    SDL_RenderCopy(ctx->renderer, ctx->player.atlas, NULL, &(SDL_Rect){
+        (int)ctx->player.hitbox.x,
+        (int)ctx->player.hitbox.y,
+        (int)ctx->player.hitbox.w,
+        (int)ctx->player.hitbox.h
+    });
 
-    SDL_RenderPresent(renderer);
+    SDL_SetRenderTarget(ctx->renderer, NULL);
+    SDL_RenderCopy(ctx->renderer, ctx->screen, NULL, NULL);
+
+    SDL_RenderPresent(ctx->renderer);
 }
 int main(int argc, char* args[]) {
-    //init
-    SDL_Init(SDL_INIT_EVERYTHING);
 
-    window = SDL_CreateWindow(
-        WINDOW_TITLE, 
-        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-        SCREEN_WIDTH * WINDOW_SCALE, 
-        SCREEN_HEIGHT * WINDOW_SCALE, 
-        0
-    );
-    renderer = SDL_CreateRenderer(window, 0, 0);
-    screen = SDL_CreateTexture(
-        renderer, 
-        SDL_PIXELFORMAT_RGBA8888, 
-        SDL_TEXTUREACCESS_TARGET, 
-        SCREEN_WIDTH, SCREEN_HEIGHT
-    );
+    //this contains all necessary game components
+    GameContext ctx;
 
+    //init window, renderer, screen
+    init(&ctx);
+
+    //
+    //beta
+
+    //FIND A BETTER PLACE FOR THIS
+    ctx.levelTiles[0][LEVEL_HEIGHT - 1].id = 1;
+    ctx.levelTiles[1][LEVEL_HEIGHT - 1].id = 1;
+    ctx.levelTiles[2][LEVEL_HEIGHT - 1].id = 1;
+    ctx.levelTiles[3][LEVEL_HEIGHT - 1].id = 1;
+    ctx.levelTiles[4][LEVEL_HEIGHT - 1].id = 1;
+
+    ctx.levelTiles[6][LEVEL_HEIGHT - 3].id = 1;
+    ctx.levelTiles[7][LEVEL_HEIGHT - 3].id = 1;
+    ctx.levelTiles[8][LEVEL_HEIGHT - 3].id = 1;
+
+
+    ctx.player = (Player){
+        .hitbox = {0, 55, 16, 16}, 
+        .atlas = IMG_LoadTexture(ctx.renderer, "res/john.png")
+    };
+
+    //game loop
     Uint32 frameStart;
     Uint32 frameElapsed;
     Uint32 deltaTime;
 
-    //beta
-    levelTiles[1][0].id = 1;
-    levelTiles[2][0].id = 1;
-    levelTiles[3][0].id = 1;
-    levelTiles[4][0].id = 1;
-    levelTiles[5][0].id = 1;
-
-    while (!done) {
+    while (!ctx.done) {
         frameStart = SDL_GetTicks();
 
-        handleEvents();
-        update(deltaTime / 1000.0f);
-        draw();
+        handleEvents(&ctx);
+        update(&ctx, deltaTime / 1000.0f);
+        draw(&ctx);
 
         frameElapsed = SDL_GetTicks() - frameStart;
         if (frameElapsed < FRAME_TARGET_TIME) {
             SDL_Delay(FRAME_TARGET_TIME - frameElapsed);
         }
-        deltaTime = frameElapsed + (SDL_GetTicks() - frameStart);
+        deltaTime = (SDL_GetTicks() - frameStart);// + frameElapsed;
     }
 
     return 0;
